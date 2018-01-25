4e980e801627eb6f89a5b91dc52966b7f3d60186
Spencer Whitehead <spencerwhitehead7@gmail.com>
2018-01-25 04:25:29 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -6,7 +6,7 @@ function init() {
   let index = 0;
   document.body.addEventListener('keydown',function(e){
 
-    if(e.which===code[index]){
+    if(e.detail===code[index]){
       index++;
       console.log(`index: ${index}`)
 
