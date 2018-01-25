b56bcadf893d05eef0759fd690ec01cc8782735c
Spencer Whitehead <spencerwhitehead7@gmail.com>
2018-01-25 03:35:04 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -6,7 +6,7 @@ function init() {
 }
 
 function onKeyDownHandler(e){
-  const key = parseInt(e.detail )
+  const key = parseInt(e.detail || e.which) )
   if(key === code[index]){
     index++
     if(index===code.length){
