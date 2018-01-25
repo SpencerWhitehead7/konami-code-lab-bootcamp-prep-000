8ee422d84b09c4a7c7ea95cac56210ca1a5f8ae4
Spencer Whitehead <spencerwhitehead7@gmail.com>
2018-01-25 03:40:02 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 var index = 0
 
-function init(e) {
+function init() {
   const key = parseInt(e.detail || e.which)
   if(key === code[index]){
     index++
@@ -14,4 +14,4 @@ function init(e) {
   }
 }
 
-body.addEventHandler('keydown', init)
+body.addEventHandler('keydown', init())
