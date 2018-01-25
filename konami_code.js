7f7e5d94f5bfb4e5a9264e11a6a0a9a5588bad5a
Spencer Whitehead <spencerwhitehead7@gmail.com>
2018-01-25 03:18:50 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+var attempt = []
 
 function init() {
   // Write your JavaScript code inside the init() function
+}
 
+function onKeyDownHandler(e){
+  for(let i=0;i<=code.length;i++){
+    if(attempt===code){
+      alert("Welcome, Lain.")
+      attempt.length=0
+    }
+    if(the input or we === code[i]){
+      attempt.push(the input or whatever)
+    }
+
+    else{
+      attempt.length=0
+      i=0
+    }
+  }
 }
