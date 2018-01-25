5d812b9d6350a0eb4f6be57dc2899ecca108ac12
Spencer Whitehead <spencerwhitehead7@gmail.com>
2018-01-25 04:00:55 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,9 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 var index = 0
 
 function init(){
-  document.body.addEventListener('keydown', code)
-}
-
+  document.body.addEventListener('keydown', function(e){
 function keydownHandler() {
   const key = parseInt(e.detail || e.which)
   if(key === code[index]){
