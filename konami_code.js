21ac9bacbfc608592c19d109f48fb2a00328dc65
Spencer Whitehead <spencerwhitehead7@gmail.com>
2018-01-25 03:39:14 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,11 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 var index = 0
 
-function init() {
-  // Write your JavaScript code inside the init() function
-}
-
-function onKeyDownHandler(e){
+function init(e) {
   const key = parseInt(e.detail || e.which)
   if(key === code[index]){
     index++
@@ -13,7 +9,7 @@ function onKeyDownHandler(e){
       alert("Welcome, Lain.")
       index=0
     }
-  } else{
+  }else{
     index=0
   }
 }
