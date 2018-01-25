e58756c5014b8720dfec46f8b9a59eecc50a0325
Spencer Whitehead <spencerwhitehead7@gmail.com>
2018-01-25 04:09:21 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -7,7 +7,7 @@ function init(){
     if(key === code[index]){
       index++
       if(index===code.length){
-        alert("Welcome, Lain.")
+        window.alert("Welcome, Lain.")
         index=0
       }
   }else{
