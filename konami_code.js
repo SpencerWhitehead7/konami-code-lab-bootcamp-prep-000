977de216cb743f94fc0983a32623dbe11c3a7afd
Spencer Whitehead <spencerwhitehead7@gmail.com>
2018-01-25 04:29:04 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ var index = 0
 
 function init(){
   document.body.addEventListener('keydown', function(e){
-    if(parseInt(e.location) === code[index]){
+    if(e.location === code[index]){
       index++
       if(index===code.length){
         window.alert("Welcome, Lain.")
